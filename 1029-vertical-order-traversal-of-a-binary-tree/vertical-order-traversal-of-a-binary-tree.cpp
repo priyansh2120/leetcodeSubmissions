9878833052279a3168ca4b3f9ba4d694a844e339
priyansh2120 <96059277+priyansh2120@users.noreply.github.com>
2024-03-05 11:33:09 +0530

    Time: 10 ms (11.87%) | Memory: 14.7 MB (22.46%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,30 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> nodes;
-        queue<pair<TreeNode*, pair<int, int>>> todo;
-        todo.push({root, {0, 0}});
-        while (!todo.empty()) {
-            auto p = todo.front();
-            todo.pop();
-            TreeNode* node = p.first;
-            int x = p.second.first, y = p.second.second;
-            nodes[x][y].insert(node -> val);
-            if (node -> left) {
-                todo.push({node -> left, {x - 1, y + 1}});
+        vector<vector<int>>ans;
+        queue<pair<TreeNode*, pair<int, int>>> q;
+        map<int, map<int, multiset<int>>>mpp;
+        q.push({root, {0,0}});
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            TreeNode *curr = it.first;
+            int cr = it.second.first;
+            int cc = it.second.second;
+            mpp[cc][cr].insert(curr->val);
+            cout<<curr->val<<" "<<cr<<" "<<cc<<endl;
+            if(curr->left){
+                q.push({curr->left,{cr+1, cc-1}});
             }
-            if (node -> right) {
-                todo.push({node -> right, {x + 1, y + 1}});
+            if(curr->right){
+                q.push({curr->right,{cr+1, cc+1}});
             }
         }
-        vector<vector<int>> ans;
-        for (auto p : nodes) {
-            vector<int> col;
-            for (auto q : p.second) {
-                col.insert(col.end(), q.second.begin(), q.second.end());
+        for(auto it:mpp){
+            int col = it.first;
+            vector<int>coldata;
+            for(auto i:it.second){
+                
+                
+                for(auto x:i.second){
+                    coldata.push_back(x);
+                }
+                
             }
-            ans.push_back(col);
+            ans.push_back(coldata);
         }
         return ans;
     }
 };
+
