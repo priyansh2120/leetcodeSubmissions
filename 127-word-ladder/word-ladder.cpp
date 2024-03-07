469910dbf435d135f4bb39eb6517ae8dedd5cc9e
priyansh2120 <96059277+priyansh2120@users.noreply.github.com>
2024-03-07 11:03:52 +0530

    Time: 937 ms (14.40%) | Memory: 38.5 MB (14.80%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> mpp(wordList.begin(), wordList.end());
-        queue<string> q;
-        q.push(beginWord);
-        int ans = 1;
-        while (!q.empty()) {
-            int n = q.size();
-            for (int i = 0; i < n; i++) {
-                string word = q.front();
-                q.pop();
-                if (word == endWord) {
-                    return ans;
-                }
-                mpp.erase(word);
-                for (int j = 0; j < word.size(); j++) {
-                    char c = word[j];
-                    for (int k = 0; k < 26; k++) {
-                        word[j] = 'a' + k;
-                        if (mpp.find(word) != mpp.end()) {
-                            q.push(word);
-                        }
-                     }
-                    word[j] = c;
-                }
-            }
-            ans++;
-        }
-        return 0;
+       unordered_set<string>mpp(wordList.begin(), wordList.end());
+       queue<pair<string, int>>q;
+       q.push({beginWord,1});
+
+       while(!q.empty()){
+           auto it = q.front();
+           string curr = it.first;
+           int ladder = it.second;
+           q.pop();
+           if(curr==endWord){
+               return ladder;
+           }
+           if(mpp.find(curr)!=mpp.end()){
+               mpp.erase(curr);
+           }
+           for(int i=0; i<curr.size(); i++){
+               char ori = curr[i];
+               for(char j ='a'; j<='z'; j++){
+                   curr[i]=j;
+                   if(mpp.find(curr)!=mpp.end()){
+                       q.push({curr, ladder+1});
+                   }
+               }
+               curr[i]=ori;
+           }
+       }
+       return 0;
     }
 };
