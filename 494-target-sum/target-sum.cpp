9af9800527ff7ae19d1d121d7bc247b93673444a
priyansh2120 <96059277+priyansh2120@users.noreply.github.com>
2024-03-16 12:35:36 +0530

    Time: 811 ms (18.23%) | Memory: 10.6 MB (92.09%) - LeetSync

---
+++ 494-target-sum/target-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    int helper(int index, int sum, int target, vector<int>&nums){
+        if(index<0){
+            if(sum==target){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        // take 
+        int l = helper(index-1, sum-nums[index], target, nums);
+        // not take 
+        int r = helper(index-1, sum+nums[index], target, nums);
+        return l+r;
+    }
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        // can give the index to either + or -
+        return helper(nums.size()-1, 0, target, nums);
+    }
+};
