f2f02af8b4f0d1af1e8da2aa3da5de1ef3dbfff3
priyansh2120 <96059277+priyansh2120@users.noreply.github.com>
2024-04-05 00:09:26 +0530

    Time: 168 ms (89.26%) | Memory: 99.4 MB (16.96%) - LeetSync

--- 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
+++ 3338-count-submatrices-with-top-left-element-and-sum-less-than-k/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -5,20 +5,17 @@ public:
         int m = grid[0].size();
 
         vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
-
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                dp[i][j]= grid[i-1][j-1]+dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1];
-            }
-        }
         int cnt=0;
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
+                dp[i][j]= grid[i-1][j-1]+dp[i-1][j]+dp[i][j-1]-dp[i-1][j-1];
                 if(dp[i][j]<=k){
                     cnt++;
                 }
             }
         }
+       
+        
         return cnt;
     }
 };
