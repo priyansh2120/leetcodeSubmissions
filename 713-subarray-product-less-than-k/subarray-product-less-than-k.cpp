41da56dc51b09132fd520a273fdf8a8c96b46a96
priyansh2120 <96059277+priyansh2120@users.noreply.github.com>
2024-04-15 22:23:33 +0530

    Time: 59 ms (48.35%) | Memory: 63.7 MB (56.20%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -1,37 +1,18 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        // approach 1 
-        // brute find all the subarray using O(n^2) and check if product is less than k
-        // more optimal
-        // use sliding window and find the subarrays with product<k and then add up no of subarray for that particular subarray
-
-        // pointers
-        int start = 0;
-        int end =0;
-        int product = 1;
-        int ans = 0;
-        int n = nums.size();
-        if(k==0){
-            return 0;
-        }
-        while(end<n){
-            // operation
-            product *= nums[end];
-
-            end++;
-
-            
-            while(product>=k && start<end){
-                product/= nums[start];
-                start++;
+        long long int product =1;
+        int s=0, e=0, ans =0;
+        while(e<nums.size()){
+            product *= nums[e];
+            while(product>=k and s<=e){
+                product/=nums[s];
+                s++;
             }
-            ans+=(end-start);
+            // cout<<s<<' '<<e<<endl;
+            ans +=(e-s+1);
+            e++;
         }
-        
-     
-
-        
         return ans;
     }
 };
