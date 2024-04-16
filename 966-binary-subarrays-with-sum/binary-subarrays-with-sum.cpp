12d2e8b71312e2c17df4ca03b55d186a0245b5eb
priyansh2120 <96059277+priyansh2120@users.noreply.github.com>
2024-04-16 12:47:54 +0530

    Time: 22 ms (79.23%) | Memory: 31.1 MB (92.36%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,43 +1,26 @@
 class Solution {
-public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        // sliding window having sum == k
-        int start =0;
-        int end = 0;
-        // if sum<k expand window
-        // if sum == k expand window and check or check while zeroes are there encompass that
-        // if sum > k start shrinking window
-
-        // naive approach
-        // find each and every subarray with 0(n^2) and calculate the sum
-
-        // proper approach use sliding window as described above
-        int sum = 0;
-        int ans = 0;
-        int n = nums.size();
-        for(int i=0; i<n; i++){
-            // operation 
-            sum += nums[i];
-
-            // condition
+private:
+    int atmost(vector<int>&nums, int goal){
+        int s=0, e=0; 
+        int ans =0;
+        int sum =0;
+        while(e<nums.size()){
+            sum+=nums[e];
+            
             while(sum>goal){
-                sum-=nums[start];
-                start++;
+                sum-=nums[s];
+                s++;
             }
-            // updation 
-            ans += i-start+1;
-        }
-        start =0;
-        sum=0;
-        for(int i=0; i<n and goal>0; i++){
-            sum+=nums[i];
-
-            while(sum>goal-1 and goal>0){
-                sum-=nums[start];
-                start++;
-            }
-            ans-=(i-start+1);
+            ans += (e-s+1);
+            e++;
         }
         return ans;
+    }
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        if(goal==0){
+            return atmost(nums, goal);
+        }
+        return atmost(nums, goal)-atmost(nums, goal-1);
     }
 };
