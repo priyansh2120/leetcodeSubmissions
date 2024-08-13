f1c227a9e5490e908e1ed8e1854412ef1bc8f4ef
Priyansh <96059277+priyansh2120@users.noreply.github.com>
2024-08-13 15:37:30 +0530

    Time: 2 ms (54.11%) | Memory: 7.6 MB (6.91%) - LeetSync

---
+++ 910-nth-magical-number/nth-magical-number.cpp
@@ -0,0 +1,37 @@
+typedef long long int ll;
+class Solution {
+private:
+    ll gcd(ll x, ll y) {
+        return y == 0 ? x : gcd(y, x % y);
+    }
+
+    ll lcm(ll x, ll y) {
+        return (x / gcd(x, y)) * y;
+    }
+
+    bool check(int n, int a, int b, ll mid) {
+        ll divByA = mid / a;
+        ll divByB = mid / b;
+        ll divByBoth = mid / lcm(a, b);
+
+        return (divByA + divByB - divByBoth) >= n;
+    }
+public:
+    int nthMagicalNumber(int n, int a, int b) {
+        ll high = 1e18;
+        ll low = 1;
+        ll ans = 0;
+        const int MOD = 1e9 + 7;
+        for(int i=0; i<100; i++){
+            ll mid = (low+high)/2;
+            if(check(n, a, b, mid)){
+                ans= mid;
+                high= mid-1;
+            }
+            else{
+                low= mid+1;
+            }
+        }
+        return ans%MOD;
+    }
+};
