a98fcc4688d979ac1b25ca9e7222bda4425d3572
Priyansh <96059277+priyansh2120@users.noreply.github.com>
2024-12-08 13:24:32 +0530

    Time: 8 ms (7.00%) | Memory: 14.8 MB (18.23%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        map<int, int>mpp;
+        for(auto it:arr){
+            mpp[it]++;
+        }
+        for(int i=0; i<arr.size(); i++){
+            if(arr[i]==0){
+                if(mpp[0]>1){
+                    return true;
+                }
+            }
+            else{
+                if(mpp.count(arr[i]*2)>0){
+                    return true;
+                }
+            }
+            
+        }
+        return false;
+    }
+};
