86673fd9ae2643993751cea75489101b48a20f16
Priyansh <96059277+priyansh2120@users.noreply.github.com>
2025-04-02 22:25:05 +0530

    Time: 7 ms (54.62%) | Memory: 127.4 MB (37.20%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -1,17 +1,20 @@
-long long dp[100000];
+
 class Solution {
 public:
-    long long dfs(vector<vector<int>>& q, int i){
-        if (i>=q.size()) return 0;
-        if (dp[i]!=-1) return dp[i];
-        long long pt=q[i][0];
-        int skip=q[i][1];
-        dp[i]=max(pt+dfs(q, i+skip+1), dfs(q, i+1));
-        return dp[i];
+    long long helper(vector<vector<int>>& questions, int index, int size, vector<long long>&dp){
+        if (index>=questions.size()){
+            return 0;
+        }
+         
+        if (dp[index]!=-1) return dp[index];
+        long long pt=questions[index][0];
+        int skip=questions[index][1];
+        dp[index]=max(pt+helper(questions, index+skip+1, size, dp), helper(questions, index+1, size, dp));
+        return dp[index];
     }
-    long long mostPoints(vector<vector<int>>& questions) {
-        const int n=questions.size();
-        memset(dp, -1, n*sizeof(long long));
-        return dfs(questions, 0);       
+    long long mostPoints(vector<vector<int>>& questions ) {
+        int n =  questions.size();
+        vector<long long>dp(n+1, -1);
+        return helper(questions, 0, n, dp);   
     }
 };
